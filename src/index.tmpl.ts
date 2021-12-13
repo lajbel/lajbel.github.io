1ddba0fe58bd5fd3bc39f224c5d3b28de8775e49
lajbel <lajbelms@gmail.com>
2021-12-13 20:44:29 -0300

    font awesome and emoji

--- src/index.tmpl.ts
+++ src/index.tmpl.ts
@@ -2,5 +2,5 @@ export const layout = "../layouts/home.njk";
 
 export const title = "LaJSite - Home";
 
-export const name = `LaJBel`
-export const bio = "Hi, i am Dani, web developer, designer (doubtful), and I like the programming, the coffee and learn new things";
+export const name = `<i class="fa-solid fa-angle-left"></i> LaJBel <i class="fa-solid fa-angle-right"></i>`
+export const bio = "Hi, i am Dani, web developer, designer (doubtful), and I like the programming, the coffee and learn new things ☕";
