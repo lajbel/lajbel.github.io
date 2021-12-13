478eb6040cb4de2dc28821a515569b9306b6fb3e
lajbel <lajbelms@gmail.com>
2021-12-13 18:51:43 -0300

    first commit

---
+++ _config.ts
@@ -0,0 +1,11 @@
+import lume from "https:/deno.land/x/lume@v1.3.0/mod.ts";
+import postcss from "https:/deno.land/x/lume@v1.3.0/plugins/postcss.ts";
+
+const site = lume({
+	src: "src/",
+	dest: "page/",
+});
+
+site.use(postcss());
+
+export default site;
---
+++ src/index.tmpl.ts
@@ -0,0 +1,6 @@
+export const layout = "../../layouts/home.njk";
+
+export const title = "LaJSite - Home";
+
+export const name = "LaJBel/Dani B. ⛵"
+export const bio = "Hi, i am LaJBel :D";
