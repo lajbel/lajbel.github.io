7827d75c8ea9ae376dcd9220edabc4172ca2682d
lajbel <lajbelms@gmail.com>
2021-12-16 00:45:34 -0300

    todo add responsive

--- src/index.tmpl.ts
+++ src/index.tmpl.ts
@@ -10,12 +10,14 @@ export const sections = [
 ];
 
 export const name = `LaJBel`;
-export const bio = "Hi, i am <span>Dani</span>, web developer, designer (doubtful), and I like the programming, the coffee and learn new things ☕";
+export const bio =
+	"Hi, i am <span>Dani</span>, web developer, designer (doubtful), and I like the programming, the coffee and learn new things ☕";
 
 export const socials = [
 	`<a href="https://github.com/lajbel" target="_blank"><i class="fa-brands fa-github"></i></a>`,
 	`<a href="https://twitter.com/lajbelms" target="_blank"><i class="fa-brands fa-twitter"></i></a>`,
-	`<a href="https://ko-fi.com/lajbel" target="_blank"><i class="fa-solid fa-mug-saucer"></i></a>`
+	`<a href="https://ko-fi.com/lajbel" target="_blank"><i class="fa-solid fa-mug-saucer"></i></a>`,
 ];
 
-export const footer = `Made with <i class="fa-solid fa-heart"></i> and <i class="fa-solid fa-fire"></i>  <i class="fa-solid fa-grip-lines-vertical"></i>  Dani Báez (c) 2021`;
+export const footer =
+	`Made with <i class="fa-solid fa-heart"></i> and <i class="fa-solid fa-fire"></i>  <i class="fa-solid fa-grip-lines-vertical"></i>  Dani Báez (c) 2021`;
