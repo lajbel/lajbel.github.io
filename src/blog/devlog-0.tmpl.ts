694f7169abfe3f60590748e85e78109213e12cfb
lajbel <lajbelms@gmail.com>
2021-12-17 08:49:00 -0300

    better devlog meta and ng link

--- src/blog/devlog-0.tmpl.ts
+++ src/blog/devlog-0.tmpl.ts
@@ -1,14 +1,14 @@
 export const layout = "layouts/entry.njk";
 export const title = "DevLog #0";
 
-export const source = ["Newgrounds", "https://newgrounds.com"];
+export const source = [
+	"Newgrounds",
+	"https://lajbel.newgrounds.com/news/post/1234663",
+];
 export const bannerImage = "/img/entrys/devlog0.jpg";
 
 export const blog = `
-I think it will do me good mentally to start with a weekly or daily devlog, when I feel like it. In the next blog I will start talking about a videogame project (an old acquaintance for the 2 people who read this and have read the previous blogs)\n
-
-<br /><br />
-
+I think it will do me good mentally to start with a weekly or daily devlog, when I feel like it. In the next blog I will start talking about a videogame project (an old acquaintance for the 2 people who read this and have read the previous blogs)\<br /><br />
 Thanks for read, drink coffee and goodbye... ☕
 `;
 
