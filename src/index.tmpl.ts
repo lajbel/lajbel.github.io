41c92c1fbbdd0f85823605fa350ddb45f88cec45
lajbel <lajbelms@gmail.com>
2021-12-19 23:34:28 -0300

    add img and itch-io

--- src/index.tmpl.ts
+++ src/index.tmpl.ts
@@ -8,6 +8,7 @@ export const bio =
 export const socials = [
 	`<a href="https://github.com/lajbel" target="_blank"><i class="fa-brands fa-github"></i></a>`,
 	`<a href="https://twitter.com/lajbelms" target="_blank"><i class="fa-brands fa-twitter"></i></a>`,
+	`<a href="https://lajbel.itch.io/" target="_blank"><i class="fa-brands fa-itch-io"></i></a>`,
 	`<a href="https://ko-fi.com/lajbel" target="_blank"><i class="fa-solid fa-mug-saucer"></i></a>`,
 ];
 
