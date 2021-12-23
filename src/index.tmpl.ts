4aa60cb8198b87bbdf51382e49be3f4443b46927
lajbel <lajbelms@gmail.com>
2021-12-23 19:01:51 -0300

    lajsite with old style (wip)

--- src/index.tmpl.ts
+++ src/index.tmpl.ts
@@ -5,9 +5,11 @@ export const name = `LaJBel`;
 export const bio =
 	"Hi, i am <span>Dani</span>, web developer, designer (doubtful), and I like the programming, the coffee and learn new things ☕";
 
+export const avatar = "img/lajvatar.png";
+
 export const socials = [
-	`<a href="https://github.com/lajbel" target="_blank"><i class="fa-brands fa-github"></i></a>`,
 	`<a href="https://twitter.com/lajbelms" target="_blank"><i class="fa-brands fa-twitter"></i></a>`,
+	`<a href="https://github.com/lajbel" target="_blank"><i class="fa-brands fa-github-alt"></i></a>`,
 	`<a href="https://lajbel.itch.io/" target="_blank"><i class="fa-brands fa-itch-io"></i></a>`,
 	`<a href="https://ko-fi.com/lajbel" target="_blank"><i class="fa-solid fa-mug-saucer"></i></a>`,
 ];
